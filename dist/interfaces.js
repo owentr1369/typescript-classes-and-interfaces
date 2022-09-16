4aea2a0a405bbad2c980a5d8ad82ff907f71c577
TranTam <tamvaa1306@gmail.com>
2022-09-16 22:28:33 +0700

    using interfaces with classes

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,10 +1,13 @@
 "use strict";
-let user1;
-user1 = {
-    name: "Max",
-    age: 30,
+class Person {
+    constructor(n) {
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + " " + this.name);
-    },
-};
+    }
+}
+let user1;
+user1 = new Person("Max");
 user1.greet("Hi there - I am");
+console.warn(user1);
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,18 +1,24 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
 
-user1 = {
-  name: "Max",
-  age: 30,
+  constructor(n: string) {
+    this.name = n;
+  }
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Max");
 
 user1.greet("Hi there - I am");
+
+console.warn(user1);
