b6fd61ccc54147e6e0260560d32c2c44ccc0fd9f
TranTam <tamvaa1306@gmail.com>
2022-09-16 09:07:23 +0700

    private and public access modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,27 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log("Department " + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfomation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Keanu");
+accounting.name = "New Department";
 accounting.describe();
 // Department Accounting
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
-accountingCopy.describe();
-// Department undefined
+accounting.printEmployeeInfomation();
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
+// accountingCopy.describe();
+// // Department undefined
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,35 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
+
   describe(this: Department) {
     console.log("Department " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInfomation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Keanu");
+
+accounting.name = "New Department";
+
 accounting.describe();
 // Department Accounting
+accounting.printEmployeeInfomation();
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
 
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
-
-accountingCopy.describe();
-// Department undefined
+// accountingCopy.describe();
+// // Department undefined
