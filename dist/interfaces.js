cc864f7b831c910377158cb675025b73449e5919
TranTam <tamvaa1306@gmail.com>
2022-09-16 22:21:48 +0700

    interfaces

---
+++ dist/interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+let user1;
+user1 = {
+    name: "Max",
+    age: 30,
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    },
+};
+user1.greet("Hi there - I am");
